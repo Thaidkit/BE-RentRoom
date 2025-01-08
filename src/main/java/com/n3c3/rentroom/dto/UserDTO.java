802d9ff7875cac0d84c6470c6792ac60234f9ec5
minh <phamnhatminh305203@gmail.com>
2025-01-08 10:48:44 +0700

    updateUser and getUserByID

--- src/main/java/com/n3c3/rentroom/dto/UserDTO.java
+++ src/main/java/com/n3c3/rentroom/dto/UserDTO.java
@@ -1,17 +1,67 @@
 package com.n3c3.rentroom.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
-    private Long id;
+    private String email;
+    private String fullName;
+    private String phone;
+    private String currentPassword;  // Mật khẩu hiện tại
+    private String newPassword;      // Mật khẩu mới
+    private String confirmPassword;  // Xác nhận mật khẩu mới
 
-    // Constructor, getter và setter
-    public UserDTO(Long id) {
-        this.id = id;
+    // Constructor mới với tham số
+
+
+    // Các getter và setter
+    public String getEmail() {
+        return email;
     }
 
-    public Long getId() {
-        return id;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
     }
 }
